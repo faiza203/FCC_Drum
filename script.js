b385391095d915c6bea3227247d46b0f9cc544bb
faiza203 <fiazfaiza981@gmail.com>
2021-05-11 12:13:13 +0500

    Add sounds

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const padBank = document.querySelector(".pad-bank");
+const dis = document.getElementById("display");
+const powerS = document.getElementById("power");
+const bankS = document.getElementById("bank");
+const range = document.getElementById("range");
+
+let power = "right";
+let bank = "left";
+
 const sounds = [
   {
     keyCode: 81,
@@ -55,18 +63,202 @@ const sounds = [
     url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
   },
 ];
+const bankOne = [
+  {
+    keyCode: 81,
+    keyTrigger: "Q",
+    id: "Heater-1",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+  },
 
-sounds.forEach((sound) => {
-  console.log(sound);
-  const newItem = document.createElement("div");
-  newItem.classList.add("drum-pad");
-  newItem.setAttribute("id", sound.id);
-  newItem.innerHTML = `   <audio
-  class="clip"
-  id=${sound.keyTrigger}
-  src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
-></audio>
-${sound.keyTrigger}
-`;
-  padBank.appendChild(newItem);
-});
+  {
+    keyCode: 87,
+    keyTrigger: "W",
+    id: "Heater-2",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+  },
+
+  {
+    keyCode: 69,
+    keyTrigger: "E",
+    id: "Heater-3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+  },
+
+  {
+    keyCode: 65,
+    keyTrigger: "A",
+    id: "Heater-4",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+  },
+
+  {
+    keyCode: 83,
+    keyTrigger: "S",
+    id: "Clap",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+  },
+
+  {
+    keyCode: 68,
+    keyTrigger: "D",
+    id: "Open-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+  },
+
+  {
+    keyCode: 90,
+    keyTrigger: "Z",
+    id: "Kick-n'-Hat",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+  },
+
+  {
+    keyCode: 88,
+    keyTrigger: "X",
+    id: "Kick",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+  },
+
+  {
+    keyCode: 67,
+    keyTrigger: "C",
+    id: "Closed-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+  },
+];
+
+const soundsTwo = [
+  {
+    keyCode: 81,
+    keyTrigger: "Q",
+    id: "Chord-1",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
+  },
+
+  {
+    keyCode: 87,
+    keyTrigger: "W",
+    id: "Chord-2",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
+  },
+
+  {
+    keyCode: 69,
+    keyTrigger: "E",
+    id: "Chord-3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
+  },
+
+  {
+    keyCode: 65,
+    keyTrigger: "A",
+    id: "Shaker",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
+  },
+
+  {
+    keyCode: 83,
+    keyTrigger: "S",
+    id: "Open-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
+  },
+
+  {
+    keyCode: 68,
+    keyTrigger: "D",
+    id: "Closed-HH",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
+  },
+
+  {
+    keyCode: 90,
+    keyTrigger: "Z",
+    id: "Punchy-Kick",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
+  },
+
+  {
+    keyCode: 88,
+    keyTrigger: "X",
+    id: "Side-Stick",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
+  },
+
+  {
+    keyCode: 67,
+    keyTrigger: "C",
+    id: "Snare",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
+  },
+];
+let rend = sounds;
+
+function renderEle() {
+  padBank.innerHTML = "";
+  rend.forEach((sound) => {
+    const newItem = document.createElement("div");
+    newItem.classList.add("drum-pad");
+    newItem.setAttribute("id", sound.id);
+    newItem.onclick = () => soundBtn(newItem);
+    newItem.innerHTML = `   <audio
+    class="clip"
+    id=${sound.keyTrigger}
+    src=${sound.url}
+  ></audio>
+  ${sound.keyTrigger}
+  `;
+    padBank.appendChild(newItem);
+  });
+}
+
+function soundBtn(e) {
+  if (power === "right") {
+    e.classList.add("right");
+    const music = e.children[0];
+    music.play();
+    dis.innerHTML = e.id;
+  }
+  if (power === "left") {
+    e.classList.add("left");
+  }
+  setInterval(() => {
+    e.classList = [];
+    e.classList.add("drum-pad");
+  }, 200);
+}
+
+function detectVolume(e) {
+  dis.innerText = `Volume : ${Math.floor(e.value)}`;
+  setInterval(() => {
+    dis.innerHTML = "&nbsp;";
+  }, 1000);
+}
+
+function changePower() {
+  if (power === "left") {
+    power = "right";
+    range.removeAttribute("disabled");
+  } else {
+    power = "left";
+    range.setAttribute("disabled", true);
+    dis.innerHTML = "&nbsp;";
+  }
+  powerS.style.float = power;
+}
+
+function changeBank() {
+  if (power === "right") {
+    bank = bank === "right" ? "left" : "right";
+    bankS.style.float = bank;
+    if (bank === "right") {
+      rend = soundsTwo;
+      renderEle();
+    } else {
+      rend = sounds;
+      renderEle();
+    }
+  }
+}
+
+renderEle();
